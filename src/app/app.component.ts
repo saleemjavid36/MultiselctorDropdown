f293d1b013609593f791cbf4d0a53ba3970bec54
saleemjavid36 <saleemjavid360@gmail.com>
2023-03-03 17:34:48 +0530

    multiselectorDropdowns

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,98 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+
+import { from, OperatorFunction } from "rxjs";
+import { delay, switchMap, map, tap } from "rxjs/operators";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements AfterViewInit{
   title = 'multiselectors';
+  @ViewChild("list") list: { filterChange: { asObservable: () => { (): any; new(): any; pipe: { (arg0: OperatorFunction<unknown, { text: string; value: number; }[]>): { (): any; new(): any; subscribe: { (arg0: (x: { text: string; value: number; }[]) => void): void; new(): any; }; }; new(): any; }; }; }; loading: boolean; } | any;
+  
+  constructor() {
+    this.data = this.source.slice();
+  }
+  public data: Array<{ text: string; value: number }>;
+
+  ngAfterViewInit() {
+    const contains = (value: any) => (s: { text: string; }) =>
+      s.text.toLowerCase().indexOf(value.toLowerCase()) !== -1;
+    this.list.filterChange
+      .asObservable()
+      .pipe(
+        switchMap((value) =>
+          from([this.source]).pipe(
+            tap(() => (this.list.loading = true)),
+            delay(200),
+            map((data) => data.filter(contains(value)))
+          )
+        )
+      )
+      .subscribe((x: { text: string; value: number; }[]) => {
+        this.data = x;
+      });
+  }
+
+
+
+  public source: Array<{ text: string; value: number }> = [
+    { text: "Item1", value: 1 },
+    { text: "Item2", value: 2 },
+    { text: "Item3", value: 3 },
+    { text: "Item4", value: 4 },
+    { text: "Item5", value: 5 },
+    { text: "Item6", value: 6 },
+    { text: "Item7", value: 7 },
+    { text: "Item8", value: 8 },
+  ];
+  public value: any = [];
+
+  public isChecked = false;
+
+  public get isIndet() {
+    return (
+      this.value.length !== 0 && this.value.length !== this.data.length
+    );
+  }
+
+  public get toggleAllText() {
+    return this.isChecked ? "Deselect All" : "Select All";
+  }
+
+  public isItemSelected(item: { value: any; }) {
+    return this.value.some((x: { value: any; }) => x.value === item.value);
+  }
+
+  public onClick() {
+    this.isChecked = !this.isChecked;
+    this.value = this.isChecked ? this.data.slice() : [];
+  }
+
+  public onValueChange() {
+    this.isChecked = this.value.length === this.data.length;
+  }
+  
+
+
+  // @ViewChild("list") list: { filterChange: { asObservable: () => { (): any; new(): any; pipe: { (arg0: OperatorFunction<unknown, { text: string; value: number; }[]>): { (): any; new(): any; subscribe: { (arg0: (x: { text: string; value: number; }[]) => void): void; new(): any; }; }; new(): any; }; }; }; loading: boolean; } | any;
+  // public source: Array<{ text: string; value: number }> = [
+  //   { text: "Item 1", value: 1 },
+  //   { text: "Item 2", value: 2 },
+  //   { text: "Item 3", value: 3 },
+  //   { text: "Item 4", value: 4 },
+  //   { text: "Item 5", value: 5 },
+  //   { text: "Item 6", value: 6 },
+    
+    
+  // ];
+  // public data: Array<{ text: string; value: number }>;
+  // public defaultItem: { text: string; value: number } = {
+  //   text: "Select item",
+  //   value: 0
+  // };
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+
+import { LabelModule } from "@progress/kendo-angular-label";
+import { InputsModule } from "@progress/kendo-angular-inputs";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +19,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonsModule,
+    BrowserAnimationsModule,
+    DropDownsModule,
+    LabelModule,
+    InputsModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
